54988f3aab880eeaf95e90e4ee742350335ccfef
Scrubi19 <scrubi.memo@yandex.ru>
2020-05-15 13:02:29 +0700

    update

--- gcd.py
+++ gcd.py
@@ -1,1 +1,1 @@
-#!/usr/bin/pythona=22b=10c = b - ad=0while a!=0 and b!=0:	if a>b:		a%=b	else:		b%=a d = 3.0 - 2.0print(a+b)
+#!/usr/bin/pythona=22b=10c = b - ad=0while a!=0 and b!=0:	if a>b:		a%=b	else:		b%=a d = 3.0 - 2.0 print(a+b)
